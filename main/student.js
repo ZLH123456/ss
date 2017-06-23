fed73b022a5d30f1559853f980490950ba50f33b
ZLH123456 <1261407236@qq.com>
2017-06-23 14:58:32 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -1,3 +1,9 @@
 let Person = require('./person')
 // Write your code here
-module.exports = Student
+module.exports = Student = {
+　　　　createNew: function(){
+　　　　　　var Student = Animal.createNew();
+　　　　　　Student.introduce=function(){ return "My name is Tom. I am 21 years old. I am a Student. I am at Class 2"; };
+　　　　　　return Student;
+　　　　}
+　　};
